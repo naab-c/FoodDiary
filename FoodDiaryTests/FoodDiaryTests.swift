c46a355a035948ba0e8f954fbec48c913daeca3d
NaabC <chitwant@gmail.com>
2025-11-11 20:47:49 -0800

    Initial Commit

---
+++ FoodDiaryTests/FoodDiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDiaryTests.swift
+//  FoodDiaryTests
+//
+//  Created by Ramandeep on 11/11/25.
+//
+
+import Testing
+@testable import FoodDiary
+
+struct FoodDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
